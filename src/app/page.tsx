572d7f5da338bcaf0f6df35e6e314bc78d631cca
thiago alves <thiago87gsa@gmail.com>
2025-05-10 10:29:17 -0300

    :sparkles: feat: create component 'sobre' - part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ComoFunciona from "@/components/como_funciona/ComoFunciona";
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 h-screen bg-midnight text-white items-center">
-      <div className="w-full max-w-[1440px] text-black">Home</div>
+      <div className="w-full max-w-[1440px] text-black">
+        <ComoFunciona/>
+      </div>
     </div>
   );
 }
