dabc8d76b318eade055184aa8b0be6e4dad0eac5
thiago alves <thiago87gsa@gmail.com>
2025-05-12 11:29:52 -0300

    :truck: chore: moving data to data folder

--- src/components/home/como_funciona/ComoFunciona.tsx
+++ src/components/home/como_funciona/ComoFunciona.tsx
@@ -1,37 +1,9 @@
 import Button from "../../Button";
 import Card from "./Card";
+import { cardComoFuncionaData } from "@/data/data";
 
 const ComoFunciona = () => {
-  const cardData = [
-    {
-      id: 1,
-      img: "/img/home/como_funciona/image1.png",
-      alt: "Gorato no computador",
-      title: "Cadastro",
-      text: "Verifique as regras e informações nos editais e inscreva-se!",
-    },
-    {
-      id: 2,
-      img: "/img/home/como_funciona/image2.png",
-      alt: "Equipe unida",
-      title: "Acolhimento",
-      text: "Participe da recepção e conheça mais sobre o EQ e sua equipe!",
-    },
-    {
-      id: 3,
-      img: "/img/home/como_funciona/image3.png",
-      alt: "Garoto escrevendo",
-      title: "Formação",
-      text: "Tenha aulas semanais de redação e escrita regularmente!",
-    },
-    {
-      id: 4,
-      img: "/img/home/como_funciona/image4.png",
-      alt: "Dois alunos se ajudando",
-      title: "Preparação Final",
-      text: "Tenha acompanhamento durante e após o período de vestibulares!",
-    },
-  ];
+  
   return (
     <section className="flex flex-col items-center my-10">
       <div className="text-center">
@@ -42,7 +14,7 @@ const ComoFunciona = () => {
         </h3>
       </div>
       <div className="flex gap-16 justify-center w-full my-10">
-        {cardData.map((item) => (
+        {cardComoFuncionaData.map((item) => (
           <div
             key={item.id}
             className={`${
