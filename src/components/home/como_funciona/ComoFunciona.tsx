bbb669cf621b0f6a08d330d3d8fd224de072aea8
thiago alves <thiago87gsa@gmail.com>
2025-05-14 19:43:22 -0300

    :boom: fix: alterando tamanhos de fontes de px para rem

--- src/components/home/como_funciona/ComoFunciona.tsx
+++ src/components/home/como_funciona/ComoFunciona.tsx
@@ -7,8 +7,8 @@ const ComoFunciona = () => {
     <section className="flex flex-col items-center w-screen min-h-screen bg-[#EBEBEB]">
       <div className="flex flex-col gap-16 items-center w-full py-16 px-6 max-w-[1440px] text-center">
         <div className="text-center">
-          <h2 className="text-[22px] md:text-[28px] font-bold mb-5 md:mb-0">Como Funciona</h2>
-          <h3 className="text-[24px] sm:text-[30px] md:text-[40px]">
+          <h2 className="text-[1.375rem] md:text-[1.75rem] font-bold mb-5 md:mb-0">Como Funciona</h2>
+          <h3 className="text-[1.5rem] sm:text-[1.875rem] md:text-[2.5rem]">
             O Escrevendo na Quebrada promove aulas de escrita e redação com foco
             em vestibulares através de quatro tarefas:
           </h3>
@@ -31,13 +31,13 @@ const ComoFunciona = () => {
           ))}
         </div>
         <div>
-          <p className="text-[20px] text-center lg:w-[976px]">
+          <p className="text-[1.25rem] text-center lg:w-[976px]">
             Vem conhecer o Escrevendo na Quebrada: um projeto gratuito, com
             alimentação nas aulas, tablets e acompanhamento psicológico para te
             apoiar em cada passo!
           </p>
           <div className="flex justify-center w-full md:w-[710px] h-[70px] mx-auto mt-16">
-            <Button className="text-[18px] sm:text-[24px]">entenda como funciona</Button>
+            <Button className="text-[1.125rem] sm:text-[1.5rem]">entenda como funciona</Button>
           </div>
         </div>
       </div>
