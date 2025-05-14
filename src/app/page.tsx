f29403405023dfdd11b7c9e3a1172aa13c3372f0
thiago alves <thiago87gsa@gmail.com>
2025-05-14 09:16:03 -0300

    :sparkles: feat: ajuste de cor de fundo padding e margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import ComoFunciona from "@/components/home/como_funciona/ComoFunciona";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-4 h-screen bg-midnight text-white items-center">
-      <div className="w-full max-w-[1440px] text-black">
+    <div className="flex flex-col gap-4 h-screen items-center">
+      <div className="">
         <ComoFunciona/>
       </div>
     </div>
--- src/components/home/como_funciona/ComoFunciona.tsx
+++ src/components/home/como_funciona/ComoFunciona.tsx
@@ -3,41 +3,42 @@ import Card from "./Card";
 import { cardComoFuncionaData } from "@/data/data";
 
 const ComoFunciona = () => {
-  
   return (
-    <section className="flex flex-col items-center my-10 h-screen">
-      <div className="text-center">
-        <h2 className="text-[28px] font-bold my-5">Como Funciona</h2>
-        <h3 className="text-[40px]">
-          O Escrevendo na Quebrada promove aulas de escrita e redação com foco
-          em vestibulares através de quatro tarefas:
-        </h3>
-      </div>
-      <div className="flex gap-16 justify-center w-full my-10">
-        {cardComoFuncionaData.map((item) => (
-          <div
-            key={item.id}
-            className={`${
-              item.id === 1 ? "border-l border-gray-200 pl-16" : ""
-            } border-r border-gray-200 pr-16`}
-          >
-            <Card
-              alt={item.alt}
-              img={item.img}
-              text={item.text}
-              title={item.title}
-            />
+    <section className="flex flex-col items-center w-screen h-screen mb-10 bg-[#EBEBEB]">
+      <div className="flex flex-col gap-16 items-center w-full max-w-[1440px] text-center">
+        <div className="text-center">
+          <h2 className="text-[28px] font-bold mt-16">Como Funciona</h2>
+          <h3 className="text-[40px]">
+            O Escrevendo na Quebrada promove aulas de escrita e redação com foco
+            em vestibulares através de quatro tarefas:
+          </h3>
+        </div>
+        <div className="flex gap-16 justify-center w-full">
+          {cardComoFuncionaData.map((item) => (
+            <div
+              key={item.id}
+              className={`${
+                item.id === 1 ? "border-l border-gray-200 pl-16" : ""
+              } border-r border-gray-200 pr-16`}
+            >
+              <Card
+                alt={item.alt}
+                img={item.img}
+                text={item.text}
+                title={item.title}
+              />
+            </div>
+          ))}
+        </div>
+        <div>
+          <p className="text-[20px] text-center w-[976px]">
+            Vem conhecer o Escrevendo na Quebrada: um projeto gratuito, com
+            alimentação nas aulas, tablets e acompanhamento psicológico para te
+            apoiar em cada passo!
+          </p>
+          <div className="flex justify-center w-[710px] h-[70px] mx-auto mt-16">
+            <Button className="text-[24px]">entenda como funciona</Button>
           </div>
-        ))}
-      </div>
-      <div>
-        <p className="text-[20px] text-center w-[976px]">
-          Vem conhecer o Escrevendo na Quebrada: um projeto gratuito, com
-          alimentação nas aulas, tablets e acompanhamento psicológico para te
-          apoiar em cada passo!
-        </p>
-        <div className="flex justify-center w-[710px] h-[70px] mx-auto mt-16">
-          <Button className="text-[24px]">entenda como funciona</Button>
         </div>
       </div>
     </section>
