aba5ff70360e16bf8db8d82dfae6c279249ae329
thiago alves <thiago87gsa@gmail.com>
2025-05-19 10:33:37 -0300

    :sparkles: feat: create section seja aluno part 2 - responsiveness - finished

--- src/components/home/seja_aluno/SejaAluno.tsx
+++ src/components/home/seja_aluno/SejaAluno.tsx
@@ -5,35 +5,45 @@ const SejaAluno = () => {
   return (
     <section className="flex flex-col items-center w-screen min-h-screen">
       <div className="flex flex-col gap-16 items-center w-full py-16 px-6 max-w-[1440px] text-center">
-        <div>
-          <h2 className="text-[3.75rem] font-bold">Seja Aluno</h2>
-          <p className="text-[1.6875rem]">
+        <div className="flex flex-col gap-2">
+          <h2 className="sm:text-[3.75rem] text-4xl font-bold">Seja Aluno</h2>
+          <p className="sm:text-[1.6875rem] text-xl">
             Nossos programas são gratuitos e pensados para apoiar você em cada
             etapa do seu desenvolvimento.
           </p>
         </div>
-        <div className="flex items-center">
+        <div className="flex flex-col lg:flex-row gap-14.5 items-center">
           <Image
             alt="dois alunos estudando"
             src="/img/home/seja_aluno/image1.png"
             width={600}
             height={600}
+            className="w-full max-w-[600px]"
           />
-          <div className="text-left">
-            <h2 className="text-[1.6875rem] font-bold">Inscrições</h2>
+          <div className="flex flex-col gap-5.5 items-center lg:items-stretch lg:text-left">
+            <h2 className="sm:text-[1.6875rem] text-2xl font-bold">Inscrições</h2>
             <p className="text-[1.125rem]">
               Acreditamos no poder do conhecimento para mudar vidas. Junte-se a
               nós e comece hoje a trilhar um caminho de novas oportunidades.
             </p>
-            <p className="text-[1.125rem]">Inscreva-se agora e dê o primeiro passo.</p>
-            <div>
+            <p className="text-[1.125rem]">
+              Inscreva-se agora e dê o primeiro passo.
+            </p>
+            <div className="w-[16.25rem] h-[2.875rem]">
               <Button>INSCREVA-SE</Button>
             </div>
           </div>
         </div>
-        <div className="flex items-center">
-          <div className="text-right">
-            <h2 className="text-[1.6875rem] font-bold">
+        <div className="flex flex-col lg:flex-row gap-14.5 items-center">
+          <Image
+            alt="dois alunos estudando"
+            src="/img/home/seja_aluno/image2.png"
+            width={600}
+            height={600}
+            className="lg:hidden w-full max-w-[600px]"
+          />
+          <div className="flex flex-col lg:items-end items-center gap-5.5 lg:text-right">
+            <h2 className="lg:text-[1.6875rem] text-2xl font-bold">
               Ficou Alguma Dúvida ?
             </h2>
             <p className="text-[1.125rem]">Estamos aqui para ajudar.</p>
@@ -41,7 +51,7 @@ const SejaAluno = () => {
               Sabemos que começar algo novo pode gerar perguntas. Reunimos aqui
               as informações mais importantes para orientar você nessa jornada.
             </p>
-            <div>
+            <div className="w-[16.25rem] h-[2.875rem] ">
               <Button>saiba mais</Button>
             </div>
           </div>
@@ -50,6 +60,7 @@ const SejaAluno = () => {
             src="/img/home/seja_aluno/image2.png"
             width={600}
             height={600}
+            className="hidden lg:flex w-full max-w-[600px]"
           />
         </div>
       </div>
