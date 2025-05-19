c853f9dd59dc78bbca9503e72036cbd0a43c1809
thiago alves <thiago87gsa@gmail.com>
2025-05-19 09:46:35 -0300

    :sparkles: feat: create section seja aluno part 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import ComoFunciona from "@/components/home/como_funciona/ComoFunciona";
-import SejaAluno from "@/components/home/seja_aluno/SejaAluno";
 import Sobre from "@/components/home/sobre/Sobre";
 
 export default function Home() {
@@ -8,7 +7,6 @@ export default function Home() {
       <div className="flex flex-col items-center w-full h-full max-w-[1440px] text-preto1">
         <Sobre />
         <ComoFunciona />
-        <SejaAluno/>
       </div>
     </main>
   );
--- src/components/home/seja_aluno/SejaAluno.tsx
+++ src/components/home/seja_aluno/SejaAluno.tsx
@@ -19,17 +19,38 @@ const SejaAluno = () => {
             width={600}
             height={600}
           />
-          <div>
-            <h2 className="">Inscrições</h2>
-            <p>
+          <div className="text-left">
+            <h2 className="text-[1.6875rem] font-bold">Inscrições</h2>
+            <p className="text-[1.125rem]">
               Acreditamos no poder do conhecimento para mudar vidas. Junte-se a
               nós e comece hoje a trilhar um caminho de novas oportunidades.
-              Inscreva-se agora e dê o primeiro passo.
             </p>
+            <p className="text-[1.125rem]">Inscreva-se agora e dê o primeiro passo.</p>
             <div>
               <Button>INSCREVA-SE</Button>
             </div>
           </div>
+        </div>
+        <div className="flex items-center">
+          <div className="text-right">
+            <h2 className="text-[1.6875rem] font-bold">
+              Ficou Alguma Dúvida ?
+            </h2>
+            <p className="text-[1.125rem]">Estamos aqui para ajudar.</p>
+            <p className="text-[1.125rem]">
+              Sabemos que começar algo novo pode gerar perguntas. Reunimos aqui
+              as informações mais importantes para orientar você nessa jornada.
+            </p>
+            <div>
+              <Button>saiba mais</Button>
+            </div>
+          </div>
+          <Image
+            alt="dois alunos estudando"
+            src="/img/home/seja_aluno/image2.png"
+            width={600}
+            height={600}
+          />
         </div>
       </div>
     </section>
