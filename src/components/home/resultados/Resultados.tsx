1713207cbdac3f4417bbb9118902f01c8bca3faf
thiago alves <thiago87gsa@gmail.com>
2025-05-20 21:54:41 -0300

    :sparkles: feat: create section Resultados part 2 - responsividade

--- src/components/home/resultados/Resultados.tsx
+++ src/components/home/resultados/Resultados.tsx
@@ -2,17 +2,17 @@ import CardNumber from "./CardNumber";
 
 const Resultados = () => {
   return (
-    <section>
-      <div className="flex flex-col gap-12 my-[10rem]">
+    <section >
+      <div className="flex flex-col gap-12 mt-10 mb-[8rem] sm:my-[10rem] px-6">
         <div className="flex flex-col gap-12">
           <h2 className="text-[2.5rem] font-bold">Nossos Resultados</h2>
-          <p className="text-[1.5rem] w-[46.125rem]">
+          <p className="text-xl sm:text-[1.5rem] lg:w-[46.125rem]">
             Impacto que transforma! Com dezenas de polos ativos, centenas de
             aprovações e prêmios conquistados, nosso cursinho popular de
             redações continua ampliando oportunidades e fortalecendo a educação.
           </p>
         </div>
-        <div className="flex w-[81.875rem] mx-auto">
+        <div className="mx-auto">
           <CardNumber />
         </div>
       </div>
