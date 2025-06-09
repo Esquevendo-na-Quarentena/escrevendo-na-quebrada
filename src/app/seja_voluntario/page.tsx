63c6983e2fb0dec187b4ed91830761c959e00bda
Iago <iagogtr@gmail.com>
2025-06-09 10:25:54 -0300

    feat(hero): começando a estrutura

--- src/app/seja_voluntario/page.tsx
+++ src/app/seja_voluntario/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Hero from '@/components/seja_voluntario/hero/Hero'
 
 const SejaVoluntario = () => {
   return (
-    <div className="flex flex-col gap-4 h-screen bg-midnight text-white items-center">
-      <div className="w-full max-w-[1440px] text-black">
-        Seja voluntario
+    <div className="flex flex-col gap-4 items-center overflow-x-hidden">
+      <div className="flex flex-col items-center w-full h-full max-w-[1440px] text-preto1">
+        <Hero/>
       </div>
     </div>
   )
