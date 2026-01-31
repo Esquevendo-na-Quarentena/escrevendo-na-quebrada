0333c73f7a6e6a831d8b48d1e43e8db6a93a1791
Mauricio Antero <mjunior447@gmail.com>
2026-01-31 15:04:48 -0300

    feat/refactor: cria badge na pagina de inscricoes; refatora componente Button, adicionando cores de hover

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,15 +1,15 @@
 interface ButtonProps {
   children: string;
   className?: string;
-  textColor?: string;
   colorVariant?: keyof typeof colorVariants;
 }
 
 const colorVariants = {
   purple: 'bg-roxo-escuro text-white1 hover:bg-roxo',
-  lightBlue: 'bg-azul-claro text-white1',
-  outlinedLightBlue: 'bg-white1 border border-azul-claro text-azul-claro hover:bg-azul-claro hover:text-white',
-  ciano: 'bg-[#10a2c7] text-white1 hover:bg-ciano',
+  outlinedDeepPurple: 'bg-transparent border-roxo border-1 text-roxo-escuro hover:bg-roxo-escuro hover:text-white1',
+  lightBlue: 'bg-azul-claro text-white1 hover:#4baebf',
+  outlinedLightBlue: 'bg-transparent border border-azul-claro text-azul-claro hover:bg-azul-claro hover:text-white',
+  ciano: 'bg-ciano text-white1 hover:bg-ciano-escuro',
 };
 
 const Button = ({
@@ -20,7 +20,7 @@ const Button = ({
 
   return (
     <button
-      className={`${colorVariants[colorVariant]} w-full px-6 rounded-4xl cursor-pointer active:opacity-85 uppercase shadow-sm/60 shadow-preto1 transition-colors ${className}`}
+      className={`${colorVariants[colorVariant]} w-full px-6 rounded-4xl cursor-pointer active:opacity-85 uppercase shadow-sm/60 shadow-preto1 transition-colors duration-200 ${className}`}
     >
       {children}
     </button>
--- src/components/inscricoes/hero/Hero.tsx
+++ src/components/inscricoes/hero/Hero.tsx
@@ -1,34 +1,42 @@
 import Button from "@/components/Button";
 import StackedCards from "./StackedCards";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { PiBookOpenBold } from "react-icons/pi";
+import Badge from "./Badge";
 
 const Hero = () => {
   return (
     <section className="min-h-[calc(100vh-97px)] w-screen flex items-center justify-center px-6 md:px-12 py-16 text-white">
-      <div className="w-full max-w-7xl mx-auto flex flex-col md:flex-row items-center px-6 py-6 pt-[6rem] justify-center gap-20">
+      <div className="w-full max-w-7xl mx-auto flex flex-col md:flex-row items-center px-6 py-6 justify-center gap-20">
         <div className="flex-1">
           <div className="mb-6">
-            <h1 className="font-bold text-3xl leading-tight tracking-normal text-preto1">
+            <Badge applicationSeason="open">Inscrições abertas</Badge>
+            <h1 className="font-bold text-6xl leading-tight tracking-normal text-preto1 mt-4">
               Escrevendo {' '}
-              <br /><span className="text-azul-claro">na Quebrada</span>
+              <br /><span className="text-ciano">na Quebrada</span>
             </h1>
           </div>
-          <div className="mb-6">
-            <p className="m-0 font-light text-xl leading-6 tracking-normal text-preto1">
-              Um programa de formação em escrita criativa que valoriza as vozes da periferia. 
+          <div className="mb-12">
+            <p className="m-0 font-normal text-xl leading-6 tracking-normal text-preto1">
+              Um programa de formação em escrita criativa que valoriza as vozes da periferia.
               Transforme suas histórias em literatura e faça parte de uma comunidade de escritores.
             </p>
           </div>
           <div className="flex gap-4">
-            <Button className="py-2 max-w-sm uppercase">
+            <Button colorVariant="ciano" className="py-2 max-w-sm uppercase">
               Inscreva-se
             </Button>
-            <Button className="py-2 max-w-sm uppercase border-1" colorVariant="outlinedLightBlue">
+            <Button colorVariant="outlinedLightBlue" className="py-2 max-w-sm uppercase border-1">
               Saiba mais
             </Button>
           </div>
+          <div className="flex justify-start items-center gap-12 text-preto1 text-xl mt-12">
+            <p className="font-normal"><FaPeopleGroup className="inline text-2xl text-ciano mr-1" /> +500 alunos formados</p>
+            <p className="font-normal"><PiBookOpenBold className="inline text-2xl text-ciano mr-1" /> +100 alunos aprovados</p>
+          </div>
         </div>
 
-        <div className="flex-1">
+        <div className="flex-1 h-100 w-100">
           <StackedCards />
         </div>
       </div>
